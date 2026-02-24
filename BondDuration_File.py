a5def5232e8a5808e951e8bafc664caad4eb4366
DaoyuLiuDL3752 <dl3752@columbia.edu>
2026-02-23 20:10:27 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,17 @@
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    n = int(m * ppy)
+    r = y / ppy
+    c = face * couponRate / ppy
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    pv_sum = 0.0
+    pv_t_sum = 0.0
+
+    for t in range(1, n + 1):
+        cf = c if t < n else c + face
+        pv = cf / ((1 + r) ** t)
+        pv_sum += pv
+        pv_t_sum += t * pv
+
+    return (pv_t_sum / pv_sum) / ppy
+
+
