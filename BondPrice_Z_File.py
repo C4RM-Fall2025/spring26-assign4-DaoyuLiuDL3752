9588c9ad4efe2b5f89a1270445dea07f6cd5e518
DaoyuLiuDL3752 <dl3752@columbia.edu>
2026-02-23 20:27:46 -0500

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,8 @@
-
-
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    c = face * couponRate
+    price = 0.0
+    n = len(times)
+    for i, (t, r) in enumerate(zip(times, yc), start=1):
+        cf = c if i < n else c + face
+        price += cf / ((1 + r) ** t)
+    return price
