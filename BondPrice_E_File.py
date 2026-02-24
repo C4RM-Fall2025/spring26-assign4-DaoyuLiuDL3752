d58c102b8091e6d1d94394627ba09d7ae39a5252
DaoyuLiuDL3752 <dl3752@columbia.edu>
2026-02-23 20:38:08 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,7 +1,8 @@
-def getBondPrice_E(face, couponRate, m, yc):
+def getBondPrice_E(face, couponRate, yc):
     c = face * couponRate
     price = 0.0
-    for t, r in enumerate(yc[:m], start=1):
+    m = len(yc)
+    for t, r in enumerate(yc, start=1):
         cf = c if t < m else c + face
         price += cf / ((1 + r) ** t)
     return price
